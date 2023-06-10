28b9e11750fd2588bb13cae406913407944f77d1
jiatuya <xinglinjia@gmail.com>
2023-06-10 18:57:49 -0500

    Remove hide folder from Git history

--- hide/php_to_hist_cc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$gene_file = $_POST['gene_file'];
-$cutoff = $_POST['cutoff'];
-$top_cutoff = $_POST['top_cutoff'];
-$go_file = $_POST['go_file'];
-$kind_of_list = $_POST['kind_of_list'];
-$output = exec("python hist_cc_web.py $gene_file $cutoff $top_cutoff $go_file $kind_of_list");
-echo $output;
-?>
